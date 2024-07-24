4e581d18b90a45eb6f617e9d7024a60cd73518b1
DK <dongwook5478@gmail.com>
2024-07-24 20:16:43 +0900

    feat: 유저 로그인 확인 기능 중 service, dto 추가

---
+++ src/main/java/org/example/login/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package org.example.login.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.login.domain.UserEntity;
+import org.example.login.dto.CustomUserDetails;
+import org.example.login.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+//    public CustomUserDetailsService(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
