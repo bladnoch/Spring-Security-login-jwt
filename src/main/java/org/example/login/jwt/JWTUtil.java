8440794e5e65ff04dff80117457d9ec12288e6ac
DK <dongwook5478@gmail.com>
2024-07-26 01:43:31 +0900

    feat: add methods of JWTUTIL class with create, and check token

--- src/main/java/org/example/login/jwt/JWTUtil.java
+++ src/main/java/org/example/login/jwt/JWTUtil.java
@@ -7,16 +7,41 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 // 키 암호화 복호화
 @Component
 public class JWTUtil {
 
+    // 키값 불러오기
     private SecretKey secretKey;
 
     public JWTUtil(@Value("${spring.jwt.secret") String secret) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
+    // 검증
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
 
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) { // timeout 확인
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+
+    // 생성
+    public String cretaJwt(String username, String role, Long expriedMs) {
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expriedMs))
+                .signWith(secretKey)
+                .compact();
+    }
 }
