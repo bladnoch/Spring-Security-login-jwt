e0045158fea637f36c222595c68d166247659706
DK <dongwook5478@gmail.com>
2024-07-28 06:17:21 +0900

    feat: JWTFilter 완료

--- src/main/java/org/example/login/jwt/JWTFilter.java
+++ src/main/java/org/example/login/jwt/JWTFilter.java
@@ -4,16 +4,77 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.login.domain.UserEntity;
+import org.example.login.dto.CustomUserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 // OncePerRequestFilter: 요청에 대해 한번만 진행
+@RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtil jwtUtil;
 
     // OncePerRequestFilter을 사용하기 위한 필수 함수
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        // request에서 Authorization 헤더를 찾음
+        String authorization = request.getHeader("Authorizaiton");
+
+        // Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            System.out.println("token ull");
+            filterChain.doFilter(request, response);
+
+            // 조검이 해당되면 매소드 종료(필수)
+            return;
+        }
+
+        System.out.println("authorization now");
+
+        //Bearer 부분 제거 후 순수 토큰만 획득
+        String token = authorization.split(" ")[1];
+
+        // 토큰 소멸 시간 검증
+        if (jwtUtil.isExpired(token)) {
+
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            // 조건이 해당되면 메소드 종료(필수)
+            return;
+        }
+
+        // 토큰에서 username과 role 획득
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        // userEntity를 생성하여 값 set
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(username);
+        // 한번만 사용되고 소멸되기 때문에 임시의 비밀번호를 넣어 userEntity를 생성
+        // token엔 비밀번호 정보가 없고(username, role)
+        // 만약 매번 생성때마다 db를 조회할 경우 효율이 좋지 않기 때문에
+        // 임시값을 넣어 토큰을 생성
+        userEntity.setPassword("temppassword");
+        userEntity.setRole(role);
+
+        // userEntity에 회원 정보 객체 담기
+        CustomUserDetails customUserDetails = new CustomUserDetails(userEntity);
+
+        // 스프링 시큐리티 인증 토큰 생성
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        // 새션에 사용자 등록
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
     }
+
 }
